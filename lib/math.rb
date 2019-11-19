4246d3c836be5d7973606f0d1c2f1cd5fef749ef
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-19 01:09:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,29 @@
 def addition(num1, num2)
-
+  num1 = "5"
+  num2 = "4"
+  sum = num1 + num2 
+  puts "#{sum}"
 end
 
 def subtraction(num1, num2)
-
+  num1 = "10"
+  num2 = "5"
+  difference = num1 - num2
+  puts "#{difference}"
 end
 
 def division(num1, num2)
-
+  num1 = "50"
+  num2 = "2"
+  quotient = num1 / num2
+  puts "#{quotient}"
 end
 
 def multiplication(num1, num2)
-
+  num1 = "4"
+  num2 = "30"
+  product = num1 * num2
+  puts "#{product}"
 end
 
 def modulo(num1, num2)
